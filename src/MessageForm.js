14d53b1075f10beea5fe515a6f12e849ab587a73
Dave Strus <dave@getfretless.com>
2018-07-03 11:35:20 -0400

    Reset the body to an empty string after submit

--- src/MessageForm.js
+++ src/MessageForm.js
@@ -8,6 +8,7 @@ class MessageForm extends Component {
   handleSubmit = (ev) => {
     ev.preventDefault()
     this.props.addMessage(this.state.body)
+    this.setState({ body: '' })
   }
 
   handleChange = (ev) => {
