299aaa07ae22a5064472f39c4b5adf8eb4f41112
Dave Strus <dave@getfretless.com>
2018-07-03 09:48:26 -0400

    Add Raleway font from Google Fonts

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 
+import Sidebar from './Sidebar'
+import Chat from './Chat'
+
 class Main extends Component {
   render() {
     return (
       <div className="Main">
-        Main
+        <Sidebar />
+        <Chat />
       </div>
     )
   }
