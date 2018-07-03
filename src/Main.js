cc6a6c217d9b528d39df765d6248d0abab8098ef
Dave Strus <dave@getfretless.com>
2018-07-03 09:55:46 -0400

    Style Main

--- src/Main.js
+++ src/Main.js
@@ -6,12 +6,18 @@ import Chat from './Chat'
 class Main extends Component {
   render() {
     return (
-      <div className="Main">
+      <div className="Main" style={styles}>
         <Sidebar />
         <Chat />
       </div>
     )
   }
+}
+
+const styles = {
+  display: 'flex',
+  alignItems: 'stretch',
+  height: '100vh',
 }
 
 export default Main
