eb526fdce2e0adb7e503b9b684fa497a6968976f
Dave Strus <dave@getfretless.com>
2018-07-05 10:13:41 -0400

    Style Chat and ChatHeader

--- src/Chat.js
+++ src/Chat.js
@@ -47,13 +47,19 @@ class Chat extends Component {
 
   render() {
     return (
-      <div className="Chat">
+      <div className="Chat" style={styles}>
         <ChatHeader />
         <MessageList messages={this.state.messages} />
         <MessageForm addMessage={this.addMessage} />
       </div>
     )
   }
+}
+
+const styles = {
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'column',
 }
 
 export default Chat
--- src/ChatHeader.js
+++ src/ChatHeader.js
@@ -2,13 +2,41 @@ import React from 'react'
 
 const ChatHeader = () => {
   return (
-    <header className="ChatHeader">
+    <header
+      className="ChatHeader"
+      style={styles.chatHeader}
+    >
       <div className="roomInfo">
-        <h2>#general</h2>
-        <p>Announcements and general chat</p>
+        <h2 style={styles.h2}>
+          #general
+        </h2>
+        <p style={styles.p}>
+          Announcements and general chat
+        </p>
       </div>
     </header>
   )
+}
+
+const styles = {
+  chatHeader: {
+    backgroundColor: '#f3f3f3',
+    borderBottom: '1px solid #ccc',
+    padding: '0.5rem 1rem',
+    display: 'flex',
+    alignItems: 'center',
+  },
+
+  h2: {
+    fontSize: '1.1rem',
+    margin: 0,
+  },
+
+  p: {
+    color: '#999',
+    margin: 0,
+    fontSize: '0.8rem',
+  }
 }
 
 export default ChatHeader
