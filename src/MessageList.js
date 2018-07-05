06e85279df20b8edd705062877ae7e8626d6486a
Dave Strus <dave@getfretless.com>
2018-07-05 10:19:17 -0400

    Style MessageList

--- src/MessageList.js
+++ src/MessageList.js
@@ -4,7 +4,20 @@ import Message from './Message'
 
 const MessageList = (props) => {
   return (
-    <div className="MessageList">
+    <div
+      className="MessageList"
+      style={styles.messageList}
+    >
+      <div
+        className="roomAnnouncement"
+        style={styles.roomAnnouncement}
+      >
+        <h3 style={styles.h3}>
+          #general
+        </h3>
+        <p>This is the very beginning of the #general room.</p>
+      </div>
+
       {
         props.messages.map(msg => (
           <Message message={msg} key={msg.id} />
@@ -12,6 +25,23 @@ const MessageList = (props) => {
       }
     </div>
   )
+}
+
+const styles = {
+  messageList: {
+    backgroundColor: 'white',
+    flex: 1,
+    paddingBottom: '1rem',
+    overflowY: 'scroll',
+  },
+
+  roomAnnouncement: {
+    padding: '2rem 1rem',
+  },
+
+  h3: {
+    fontSize: '1.5rem',
+  },
 }
 
 export default MessageList
