f70bf04234146bac438050a418475d3462ef5019
Dave Strus <dave@getfretless.com>
2018-07-05 11:58:53 -0400

    Add and configure Firebase

---
+++ src/base.example.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+
+// Initialize Firebase
+const config = {
+  apiKey: "YOUR API KEY",
+  authDomain: "YOUR AUTH DOMAIN",
+  databaseURL: "YOUR DATABASE URL",
+  projectId: "YOUR PROJECT ID",
+  storageBucket: "YOUR STORAGE BUCKET",
+  messagingSenderId: "YOUR MESSAGING SENDER ID"
+}
+
+const app = firebase.initializeApp(config)
