bfd587408c508bbd1962a4e0503e75a5c6183490
Dave Strus <dave@getfretless.com>
2018-07-09 10:14:08 -0400

    Use avatars from authentication services

--- src/Avatar.js
+++ src/Avatar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Avatar = ({ user }) => {
-  const imageUrl = `https://api.adorable.io/avatars/40/${user.email}.png`
+  const imageUrl = user.photoUrl || `https://api.adorable.io/avatars/40/${user.email}.png`
 
   return (
     <div
@@ -20,6 +20,7 @@ const styles = {
   fontSize: '1rem',
   borderRadius: '20px',
   marginRight: '0.5rem',
+  backgroundSize: '40px 40px',
 }
 
 export default Avatar
